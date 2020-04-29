9238d0c66b7bd2723373081724fa0aabe9c879c9
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2020-04-29 00:15:24 -0300

    Add swagger

---
+++ src/main/java/com/alexandrecampos/scramblewebapi/config/Swagger2Config.java
@@ -0,0 +1,30 @@
+package com.alexandrecampos.scramblewebapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.alexandrecampos.scramblewebapi.controller"))
+				.paths(PathSelectors.regex("/.*")).build().apiInfo(apiEndPointsInfo());
+	}
+
+	private ApiInfo apiEndPointsInfo() {
+		return new ApiInfoBuilder().title("Scramble REST API").description("An API for exposing cubing scrambles.")
+				.contact(new Contact("Alexandre Campos", "https://github.com/campos20", "camposalexandreh@gmail.com"))
+				.license("GPLv3").build();
+	}
+}
