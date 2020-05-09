dfbd6887c03bb6e6eacaa043412ead34d6332942
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2020-05-09 02:04:43 -0300

    Enable CrossOrigin

--- src/main/java/com/alexandrecampos/scramblewebapi/controller/ScrambleController.java
+++ src/main/java/com/alexandrecampos/scramblewebapi/controller/ScrambleController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import com.alexandrecampos.scramblewebapi.vo.ScrambleVo;
 
 import io.swagger.annotations.ApiOperation;
 
+@CrossOrigin
 @RestController
 @RequestMapping(path = "scramble")
 public class ScrambleController {
